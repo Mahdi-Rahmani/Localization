9330c2a095e4f3d1b8dc1e6e61d21d057780e600
Mahdi-Rahmani <mah.rah79@yahoo.com>
2023-08-03 11:13:07 +0330

    some changes in lidar odometry is applied

--- EKF/lidar.py
+++ EKF/lidar.py
@@ -17,7 +17,7 @@ class LIDAR:
         self.lidar_range = 100.0
         self.points_per_second = 1000000
         self.delta = 0.1
-        self.NOISE_STDDEV = 0.02
+        self.NOISE_STDDEV = 0.01
 
         # Lidar odometry variables
         self.counter = 0
@@ -35,11 +35,6 @@ class LIDAR:
         # with down sampling
         self.voxel_size = 0.5
 
-        # vehicle position obtained by Lidar
-        self.x = None
-        self.y = None
-        self.z = None
-
         # Lidar blueprint
         lidar_bp = self.generate_lidar_bp(blueprint_library)
 
@@ -47,7 +42,7 @@ class LIDAR:
         self.lidar = self.world.spawn_actor(
             blueprint=lidar_bp,
             transform=lidar_transform,
-            attach_to=self.vehicle
+            attach_to=vehicle
         )
 
     def generate_lidar_bp(self, blueprint_library):
@@ -75,24 +70,89 @@ class LIDAR:
 
         lidar_bp.set_attribute('points_per_second', str(self.points_per_second))
         return lidar_bp
-    
-
-    @staticmethod
-    def sensor_callback(weak_self, lidar_data):
-        self = weak_self()
-        if not self:
-            return
-        pose = self.odometry(lidar_data)
-
-        self.x, self.y, self.z = pose
 
 
+    def create_transform(self, p, R):
+        self.counter += 1
+        if self.counter==1:
+            self.p_prev = p
+            self.R_01 = R
+        else:
+            R_02 = R
+            self.p = p
+            translation = self.p - self.p_prev
+            orientation = np.linalg.inv(self.R_01)@R_02
+            self.R_01 = R_02
 
+            # create T
+            self.T_12 = np.eye(4)
+            self.T_12[:3,:3] = orientation
+            self.T_12[:3,3] = translation
 
+    def lidar_odometry(self, point_cloud):
+        data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
+        data = np.reshape(data, (int(data.shape[0] / 4), 4))
 
+        points = data[:, :-1]
+        points = self.filter_points_fast(points)
 
+        if self.counter == 1:
+            self.source = o3d.geometry.PointCloud()
+            self.source.points = o3d.utility.Vector3dVector(points)
+            if self.voxel_size != 0:
+                self.source = self.source.voxel_down_sample(voxel_size= self.voxel_size)
+        else:
+            # first we should prepare traget pcl
+            target = o3d.geometry.PointCloud()
+            target.points = o3d.utility.Vector3dVector(points)
+            target_norm = None
+            if self.voxel_size == 0:
+                # without down sampling
+                target_norm = target
+                target_norm.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.8, max_nn=10))
+            else:
+                # with down sampling
+                target = target.voxel_down_sample(voxel_size= self.voxel_size)
+                target_norm = target
+                target_norm.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1.15, max_nn=10))
 
-    def odometry(self, point_cloud):
+            T_12_optimized = self.registration(target_norm)
+            new_xyz = self.p + T_12_optimized[:3,3]
+
+            return new_xyz.tolist()
+            
+    def filter_points_fast(self, points_array):
+        # non-ground selection
+        z_condition = np.logical_and(points_array[:, 2] > -0.2, points_array[:, 2] < 0.3)
+        x_condition = np.logical_and(points_array[:, 0] > -25, points_array[:, 0] < 20)
+        filtered_indices = np.logical_and(z_condition, x_condition)
+        
+        y_condition2 = np.logical_or(points_array[:, 1] <= -1.2, points_array[:, 1] >= 1.2)
+        x_condition2 = np.logical_or(points_array[:, 0] <= -2.8, points_array[:, 0] >= 2)
+        exclude_condition = np.logical_or(y_condition2, x_condition2)
+        
+        filtered_indices = np.logical_and(exclude_condition, filtered_indices)
+        
+        filtered_points = points_array[filtered_indices]
+        return filtered_points
+
+    def registration(self, target_norm):
+        # we should give an initial transform matrix to registration algorithm
+        initial_transform = np.linalg.inv(self.T_12)
+        
+        # point to plane ICP registraion algorithm
+        reg_p2l = o3d.pipelines.registration.registration_icp(
+        self.source, target_norm, self.threshold, init =initial_transform,
+        estimation_method= o3d.pipelines.registration.TransformationEstimationPointToPlane(),
+        criteria = o3d.pipelines.registration.ICPConvergenceCriteria(relative_fitness=0.0000001,
+                                                                        relative_rmse=0.01,
+                                                                        max_iteration=300))
+        T_12 = np.linalg.inv(reg_p2l.transformation)
+        return T_12
+
+    def destroy(self):
+        self.lidar.destroy()
+    '''def odometry(self, point_cloud):
         self.counter += 1
         data = np.copy(np.frombuffer(point_cloud.raw_data, dtype=np.dtype('f4')))
         data = np.reshape(data, (int(data.shape[0] / 4), 4))
@@ -100,7 +160,7 @@ class LIDAR:
         # Isolate the 3D data
         points = data[:, :-1]
         points = self.filter_points_fast(points)
-
+        
         if 1 <= self.counter:
             # get lidar transform of current step
             lidar_tran = point_cloud.transform
@@ -187,4 +247,4 @@ class LIDAR:
         # The source we used here is a target for next iteration
         self.source = target
 
-        return reg_p2l.transformation
+        return reg_p2l.transformation'''
